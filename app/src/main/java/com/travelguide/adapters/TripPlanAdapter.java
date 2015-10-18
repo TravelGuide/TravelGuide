dce21720ece10948fed33a0c18b907437313bc47
Rafael Camargo <rafagcamargo@gmail.com>
2015-10-18 11:58:33 -0700

    - Basics of Stream Fragment.

--- app/src/main/java/com/travelguide/adapters/TripPlanAdapter.java
+++ app/src/main/java/com/travelguide/adapters/TripPlanAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.travelguide.R;
@@ -34,7 +36,13 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
     public void onBindViewHolder(ViewHolder holder, int position) {
         TripPlan tripPlan = mTripPlans.get(position);
 
-        holder.tvPlanName.setText("Trip to NYC" + position);
+        holder.tvTotalTravelTime.setText("3 days");
+        holder.tvGroupType.setText("Family");
+        holder.tvTravelSeason.setText("Winter");
+        holder.tvTravelMonth.setText("Jan");
+        holder.tvPlanName.setText("Trip to NYC " + position);
+        holder.ratingBar.setRating(2);
+        holder.tvReviewCount.setText("1500 reviews");
     }
 
     @Override
@@ -43,12 +51,26 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTotalTravelTime;
+        TextView tvGroupType;
+        TextView tvTravelSeason;
+        TextView tvTravelMonth;
+        ImageView ivPlace;
         TextView tvPlanName;
+        RatingBar ratingBar;
+        TextView tvReviewCount;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
+            tvTotalTravelTime = (TextView) itemView.findViewById(R.id.tvTotalTravelTime);
+            tvGroupType = (TextView) itemView.findViewById(R.id.tvGroupType);
+            tvTravelSeason = (TextView) itemView.findViewById(R.id.tvTravelSeason);
+            tvTravelMonth = (TextView) itemView.findViewById(R.id.tvTravelMonth);
+            ivPlace = (ImageView) itemView.findViewById(R.id.ivPlace);
             tvPlanName = (TextView) itemView.findViewById(R.id.tvPlanName);
+            ratingBar = (RatingBar) itemView.findViewById(R.id.ratingBar);
+            tvReviewCount = (TextView) itemView.findViewById(R.id.tvReviewCount);
         }
     }
 }
