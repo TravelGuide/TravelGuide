0f9156a11272de9e998b10cf0aca162cc2426959
kprav <praveen.86@gmail.com>
2015-10-25 21:44:27 -0700

    SearchListFragment and updates to handling backstack

--- app/src/main/java/com/travelguide/fragments/TripBaseFragment.java
+++ app/src/main/java/com/travelguide/fragments/TripBaseFragment.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class TripBaseFragment extends Fragment {
 
     protected void setTitle(String title) {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        if (((AppCompatActivity) getActivity()).getSupportActionBar() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        }
     }
 }
