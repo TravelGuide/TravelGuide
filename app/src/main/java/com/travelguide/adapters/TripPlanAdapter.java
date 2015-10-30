df1e6f1bd6f461120c5686c5e4f3f7b8142998b1
Hemanth <tammareddy53@gmail.com>
2015-10-30 15:45:24 -0700

    Updated
    Updated

--- app/src/main/java/com/travelguide/adapters/TripPlanAdapter.java
+++ app/src/main/java/com/travelguide/adapters/TripPlanAdapter.java
@@ -3,14 +3,19 @@ package com.travelguide.adapters;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 import com.travelguide.R;
 import com.travelguide.models.TripPlan;
 
@@ -20,6 +25,7 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
 
     private final Context mContext;
     private final List<TripPlan> mTripPlans;
+    //private final Target target;
 
     public TripPlanAdapter(List<TripPlan> mTripPlans, Context context) {
         this.mTripPlans = mTripPlans;
@@ -51,11 +57,56 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
 
         holder.ivPlace.setImageResource(R.drawable.city_placeholder);
 
+
+        final Target target  = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                //holder.ivPlace.setImageBitmap(bitmap);
+                Palette.from(bitmap).maximumColorCount(16).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        // Get the "vibrant" color swatch based on the bitmap
+                        Palette.Swatch vibrant = palette.getVibrantSwatch();
+                        if (vibrant != null) {
+                            // Set the background color of a layout based on the vibrant color
+                            holder.ivPlace.setBackgroundColor(vibrant.getPopulation());
+                            //holder.setBackgroundColor(vibrant.getRgb());
+                            // Update the title TextView with the proper text color
+                            holder.rlTripPlan.setBackgroundColor(vibrant.getRgb());
+                            holder.tvPlanName.setTextColor(vibrant.getTitleTextColor());
+                            holder.tvTotalTravelTime.setTextColor(vibrant.getTitleTextColor());
+                            holder.tvGroupType.setTextColor(vibrant.getBodyTextColor());
+                            holder.tvTravelSeason.setTextColor(vibrant.getBodyTextColor());
+                            holder.tvTravelMonth.setTextColor(vibrant.getBodyTextColor());
+                            holder.tvTravelMonth.setTextColor(vibrant.getBodyTextColor());
+                            holder.tvTravelMonth.setTextColor(vibrant.getBodyTextColor());
+                            holder.tvTotalTravelTime.setTextColor(vibrant.getBodyTextColor());
+
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+            }
+        };
+
         Picasso.with(mContext)
                 .load(tripPlan.getCityImageUrl())
                 .placeholder(R.drawable.city_placeholder)
                 .resize(0, 600)
                 .into(holder.ivPlace);
+
+        Picasso.with(mContext)
+                .load(tripPlan.getCityImageUrl())
+                .placeholder(R.drawable.city_placeholder)
+                .resize(0, 600)
+                .into(target);
 
         holder.ivFavIcon.setSelected(tripPlan.isFavorited());
     }
@@ -77,6 +128,7 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
         ImageView ivPlace;
         TextView tvPlanName;
         ImageView ivFavIcon;
+        RelativeLayout rlTripPlan;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -88,6 +140,7 @@ public class TripPlanAdapter extends RecyclerView.Adapter<TripPlanAdapter.ViewHo
             ivPlace = (ImageView) itemView.findViewById(R.id.ivPlace);
             tvPlanName = (TextView) itemView.findViewById(R.id.tvPlanName);
             ivFavIcon = (ImageView) itemView.findViewById(R.id.ivFavorite);
+            rlTripPlan = (RelativeLayout) itemView.findViewById(R.id.rlTripPlan);
             ivFavIcon.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
