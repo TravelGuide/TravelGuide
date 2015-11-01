b9de130f22a023a3a50b0fa428e7dbbf58ec2077
kprav <praveen.86@gmail.com>
2015-11-01 01:41:58 -0700

    ViewPager in ProfileFragment

--- app/src/main/java/com/travelguide/fragments/ProfileFragment.java
+++ app/src/main/java/com/travelguide/fragments/ProfileFragment.java
@@ -5,8 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,21 +17,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.parse.FindCallback;
-import com.parse.ParseException;
+import com.astuetz.PagerSlidingTabStrip;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.squareup.picasso.Picasso;
 import com.travelguide.R;
 import com.travelguide.adapters.TripPlanAdapter;
-import com.travelguide.decorations.VerticalSpaceItemDecoration;
+import com.travelguide.adapters.TripPlanPagerAdapter;
 import com.travelguide.helpers.DeviceDimensionsHelper;
 import com.travelguide.helpers.ItemClickSupport;
-import com.travelguide.helpers.NetworkAvailabilityCheck;
 import com.travelguide.models.TripPlan;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -47,13 +42,12 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ProfileFragment extends Fragment {
     private static final String TAG = ProfileFragment.class.getSimpleName();
 
+    private View view;
+
     private CircleImageView ivProfilePic;
     private ImageView ivCoverPic;
     private TextView tvName;
     private TextView tvEmail;
-    private TextView tvEmpty;
-    private RecyclerView rvTripPlans;
-    private MaterialDialog progressDialog;
 
     private String userObjectId = null;
     private String name = null;
@@ -64,8 +58,10 @@ public class ProfileFragment extends Fragment {
     private SharedPreferences userInfo;
 
     private OnFragmentInteractionListener mListener;
-    private TripPlanAdapter mTripPlanAdapter;
-    private List<TripPlan> mTripPlans;
+
+    private ViewPager vpPager;
+    private TripPlanPagerAdapter viewPagerAdapter;
+    private PagerSlidingTabStrip tabsStrip;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -82,7 +78,7 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
         ivCoverPic = (ImageView) view.findViewById(R.id.ivCoverPicInProfile);
         ivProfilePic = (CircleImageView) view.findViewById(R.id.ivProfilePicInProfile);
         tvName = (TextView) view.findViewById(R.id.tvNameInProfile);
@@ -95,37 +91,31 @@ public class ProfileFragment extends Fragment {
         Picasso.with(getContext()).load(profilePicUrl).into(ivProfilePic);
         Picasso.with(getContext()).load(coverPicUrl).resize(DeviceDimensionsHelper.getDisplayWidth(getActivity()), 0).into(ivCoverPic);
 
-        mTripPlans = new ArrayList<>();
-        mTripPlanAdapter = new TripPlanAdapter(mTripPlans, getContext());
-
-        rvTripPlans = (RecyclerView) view.findViewById(R.id.rvTripPlansInProfile);
-        rvTripPlans.setAdapter(mTripPlanAdapter);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        rvTripPlans.setLayoutManager(layoutManager);
-
-        RecyclerView.ItemDecoration itemDecoration = new VerticalSpaceItemDecoration(20, true, true);
-        rvTripPlans.addItemDecoration(itemDecoration);
-
-        ItemClickSupport.addTo(rvTripPlans).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
-            @Override
-            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
-                String tripPlanObjectId = mTripPlanAdapter.get(position).getObjectId();
-                if (mListener != null) {
-                    mListener.onTripPlanItemSelected(tripPlanObjectId);
-                }
-            }
-        });
-
-        tvEmpty = (TextView) view.findViewById(R.id.tvEmptyInProfile);
-
-        progressDialog = new MaterialDialog.Builder(getContext())
-                .title(R.string.loading_plans)
-                .content(R.string.please_wait)
-                .progress(true, 0)
-                .build();
+        loadFragments(view);
 
         return view;
+    }
+
+    private void loadFragments(View view) {
+        // Get the viewpager and setup a PageChangeListener
+        vpPager = (ViewPager) view.findViewById(R.id.viewpager);
+        // Get the view pager adapter for the pager
+        viewPagerAdapter = new TripPlanPagerAdapter(getActivity().getSupportFragmentManager());
+        vpPager.setAdapter(viewPagerAdapter);
+        // Find the sliding tabstrips
+        tabsStrip = (PagerSlidingTabStrip) view.findViewById(R.id.tabs);
+        // Attach the tabstrip to the view pager
+        tabsStrip.setViewPager(vpPager);
+        setupPageChangeListener();
+    }
+
+    private void setupPageChangeListener() {
+        tabsStrip.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(final int position) {
+                Log.d(TAG, "PageChanged");
+            }
+        });
     }
 
     @Override
@@ -149,52 +139,6 @@ public class ProfileFragment extends Fragment {
         coverPicUrl = userInfo.getString("coverPicUrl", "missing");
     }
 
-    private void loadTripPlansFromRemote() {
-        ParseQuery<TripPlan> query = ParseQuery.getQuery(TripPlan.class);
-        query.whereEqualTo("createdUserId", userObjectId);
-        query.findInBackground(new FindCallback<TripPlan>() {
-            @Override
-            public void done(List<TripPlan> tripPlans, ParseException e) {
-                progressDialog.dismiss();
-                if (e == null) {
-                    hideEmptyView();
-                    mTripPlans.clear();
-                    mTripPlans.addAll(tripPlans);
-                    mTripPlanAdapter.notifyDataSetChanged();
-                    savingOnDatabase(tripPlans);
-                } else {
-                    Log.d(TAG, "Error: " + e.getMessage());
-                }
-                if (mTripPlans.size() == 0) {
-                    showEmptyView();
-                }
-            }
-        });
-    }
-
-    private void loadTripPlansFromDatabase() {
-        ParseQuery<TripPlan> query = ParseQuery.getQuery(TripPlan.class);
-        query.whereEqualTo("createdUserId", userObjectId);
-        query.fromLocalDatastore();
-        query.findInBackground(new FindCallback<TripPlan>() {
-            @Override
-            public void done(List<TripPlan> tripPlans, ParseException e) {
-                progressDialog.dismiss();
-                if (e == null) {
-                    hideEmptyView();
-                    mTripPlans.clear();
-                    mTripPlans.addAll(tripPlans);
-                    mTripPlanAdapter.notifyDataSetChanged();
-                } else {
-                    Log.d(TAG, "Error: " + e.getMessage());
-                }
-                if (mTripPlans.size() == 0) {
-                    showEmptyView();
-                }
-            }
-        });
-    }
-
     private void savingOnDatabase(List<TripPlan> tripPlans) {
         ParseObject.pinAllInBackground(tripPlans);
     }
@@ -206,12 +150,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        progressDialog.show();
-        if (NetworkAvailabilityCheck.networkAvailable(getActivity())) {
-            loadTripPlansFromRemote();
-        } else {
-            loadTripPlansFromDatabase();
-        }
+        if (view != null)
+            loadFragments(view);
     }
 
     @Override
@@ -241,13 +181,15 @@ public class ProfileFragment extends Fragment {
         mListener = null;
     }
 
-    private void showEmptyView() {
-        rvTripPlans.setVisibility(View.GONE);
-        tvEmpty.setVisibility(View.VISIBLE);
-    }
-
-    private void hideEmptyView() {
-        rvTripPlans.setVisibility(View.VISIBLE);
-        tvEmpty.setVisibility(View.GONE);
+    public void addItemClickSupport(RecyclerView rvTripPlans, final TripPlanAdapter mTripPlanAdapter) {
+        ItemClickSupport.addTo(rvTripPlans).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                String tripPlanObjectId = mTripPlanAdapter.get(position).getObjectId();
+                if (mListener != null) {
+                    mListener.onTripPlanItemSelected(tripPlanObjectId);
+                }
+            }
+        });
     }
 }
