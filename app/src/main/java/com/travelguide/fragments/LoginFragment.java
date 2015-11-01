5ac262a1480255ce4dfed19732d9b225862fb1f1
kprav <praveen.86@gmail.com>
2015-11-01 10:54:36 -0800

    Put back Picasso to prevent App Crash

--- app/src/main/java/com/travelguide/fragments/LoginFragment.java
+++ app/src/main/java/com/travelguide/fragments/LoginFragment.java
@@ -34,6 +34,8 @@ import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 import com.travelguide.R;
 import com.travelguide.helpers.DeviceDimensionsHelper;
 import com.travelguide.helpers.NetworkAvailabilityCheck;
@@ -272,37 +274,37 @@ public class LoginFragment extends DialogFragment {
 
                             if (!Preferences.readString(getContext(), Preferences.User.PROFILE_PIC_URL).equals(profilePicUrl)) {
                                 updateCoverPicUrl = false;
-//                                Picasso.with(getContext()).load(profilePicUrl).into(ivProfilePic, new Callback() {
-//                                    @Override
-//                                    public void onSuccess() {
-//                                        Preferences.writeString(getContext(), Preferences.User.PROFILE_PIC_URL, profilePicUrl);
-//                                        if (coverPicUrl != null) {
-//                                            if (!Preferences.readString(getContext(), Preferences.User.COVER_PIC_URL).equals(coverPicUrl)) {
-//                                                Picasso.with(getContext()).load(coverPicUrl).into(ivCoverPic, new Callback() {
-//                                                    @Override
-//                                                    public void onSuccess() {
-//                                                        Preferences.writeString(getContext(), Preferences.User.COVER_PIC_URL, coverPicUrl);
-//                                                        saveOrUpdateParseUser(requestType);
-//                                                    }
-//
-//                                                    @Override
-//                                                    public void onError() {
-//                                                        saveOrUpdateParseUser(requestType);
-//                                                    }
-//                                                });
-//                                            }
-//                                        } else {
-//                                            ivCoverPic.setImageResource(android.R.color.transparent);
-//                                            saveOrUpdateParseUser(requestType);
-//                                        }
-//                                    }
-//
-//                                    @Override
-//                                    public void onError() {
-//                                        // TODO: Handle Error
-//                                        // saveOrUpdateParseUser(requestType);
-//                                    }
-//                                });
+                                Picasso.with(getContext()).load(profilePicUrl).into(ivProfilePic, new Callback() {
+                                    @Override
+                                    public void onSuccess() {
+                                        Preferences.writeString(getContext(), Preferences.User.PROFILE_PIC_URL, profilePicUrl);
+                                        if (coverPicUrl != null) {
+                                            if (!Preferences.readString(getContext(), Preferences.User.COVER_PIC_URL).equals(coverPicUrl)) {
+                                                Picasso.with(getContext()).load(coverPicUrl).into(ivCoverPic, new Callback() {
+                                                    @Override
+                                                    public void onSuccess() {
+                                                        Preferences.writeString(getContext(), Preferences.User.COVER_PIC_URL, coverPicUrl);
+                                                        saveOrUpdateParseUser(requestType);
+                                                    }
+
+                                                    @Override
+                                                    public void onError() {
+                                                        saveOrUpdateParseUser(requestType);
+                                                    }
+                                                });
+                                            }
+                                        } else {
+                                            ivCoverPic.setImageResource(android.R.color.transparent);
+                                            saveOrUpdateParseUser(requestType);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError() {
+                                        // TODO: Handle Error
+                                        // saveOrUpdateParseUser(requestType);
+                                    }
+                                });
                             } else {
                                 saveOrUpdate = false;
                             }
@@ -312,17 +314,17 @@ public class LoginFragment extends DialogFragment {
                                 if (coverPicUrl != null) {
                                     if (!Preferences.readString(getContext(), Preferences.User.COVER_PIC_URL).equals(coverPicUrl)) {
                                         saveOrUpdate = true;
-//                                        Picasso.with(getContext()).load(coverPicUrl).resize(getView().getWidth(), 0).into(ivCoverPic, new Callback() {
-//                                            @Override
-//                                            public void onSuccess() {
-//                                                Preferences.writeString(getContext(), Preferences.User.COVER_PIC_URL, coverPicUrl);
-//                                                saveOrUpdateParseUser(requestType);
-//                                            }
-//                                            @Override
-//                                            public void onError() {
-//                                                saveOrUpdateParseUser(requestType);
-//                                            }
-//                                        });
+                                        Picasso.with(getContext()).load(coverPicUrl).resize(getView().getWidth(), 0).into(ivCoverPic, new Callback() {
+                                            @Override
+                                            public void onSuccess() {
+                                                Preferences.writeString(getContext(), Preferences.User.COVER_PIC_URL, coverPicUrl);
+                                                saveOrUpdateParseUser(requestType);
+                                            }
+                                            @Override
+                                            public void onError() {
+                                                saveOrUpdateParseUser(requestType);
+                                            }
+                                        });
                                     } else {
                                         saveOrUpdate = false;
                                     }
