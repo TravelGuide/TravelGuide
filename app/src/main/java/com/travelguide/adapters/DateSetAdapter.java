c9a866e5701f19fd7ce6de3a00b45d267d95ee02
Rafael Camargo <rafagcamargo@gmail.com>
2015-11-03 21:35:37 -0800

    Plan creation UI.

--- app/src/main/java/com/travelguide/adapters/DateSetAdapter.java
+++ app/src/main/java/com/travelguide/adapters/DateSetAdapter.java
@@ -1,8 +1,13 @@
 package com.travelguide.adapters;
 
 import android.app.DatePickerDialog;
+import android.widget.DatePicker;
 
 import java.io.Serializable;
 
-public abstract class DateSetAdapter implements DatePickerDialog.OnDateSetListener, Serializable {
+public class DateSetAdapter implements DatePickerDialog.OnDateSetListener, Serializable {
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+    }
 }
